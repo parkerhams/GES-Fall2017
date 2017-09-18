f488757f08fdf6d6881816848b8072f6dc07309a
Parker Hamilton <PARKER.HAMILTON@iam.local>
2017-09-18 18:14:29 -0500

    Monday 9-18 - Added player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        Debug.Log("Called from Start.");	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Debug.Log("Called from Update.");
+    }
+}
