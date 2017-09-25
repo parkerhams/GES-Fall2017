0fa1bc062b0a8041429046bf969c7f6cb8b60b58
Parker Hamilton <PARKER.HAMILTON@iam.local>
2017-09-25 18:19:12 -0500

    Class 9-25: Jump Fix, Added Movement Adjustments, Collisions

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,8 +7,23 @@ public class PlayerMovement : MonoBehaviour {
     [SerializeField]
     float movementSpeed = 1;
 
+    [SerializeField]
+    float jumpStrength = 5;
+    
+    private bool isOnGround;
+
+    [SerializeField]
+    Transform groundDetectCenterPoint;
+
+    [SerializeField]
+    float groundDetectRadius = 0.2f;
+
+    [SerializeField]
+    LayerMask whatCountsAsGround;
+
     Rigidbody2D myRigidbody;
-	// Use this for initialization
+	
+    // Use this for initialization
 	void Start ()
     {
         //This code teleports the game object to a new location
@@ -17,32 +33,57 @@ public class PlayerMovement : MonoBehaviour {
     }
 	
 	// Update is called once per frame
-	void Update ()
+	private void Update ()
     {
+        UpdateIsOnGround();
+        Move();
+        Jump();
+        
+    }
 
-        //Don't use Input.Getkey, use GetButton and GetAxis.
+    private void UpdateIsOnGround()
+    {
+        //Draws circle, returns Collider2DArray[] if it fits layerMask of Ground
+        //We must save that array to do anything with it--
+        Collider2D[] groundObjects = 
+        Physics2D.OverlapCircleAll(groundDetectCenterPoint.position, groundDetectRadius, whatCountsAsGround);
+        //This will be true if there's anything inside of it, it will be false if nothing is in it
+        isOnGround = groundObjects.Length > 0;
+    }
 
-        //if (Input.GetKey(KeyCode.D))
-        //{
-        //    transform.Translate(new Vector3(0.1f, 0, 0));
-        //}
+    //Must be written this exact way for Unity's 2D engine to call automatically
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isOnGround = true;
+    }
 
-        //if (Input.GetKey(KeyCode.A))
-        //{
-        //    transform.Translate(new Vector3(-0.1f, 0, 0));
-        //}
-
-        float horizontalInput = Input.GetAxis("Horizontal");
-        myRigidbody.velocity = new Vector2(horizontalInput * movementSpeed, myRigidbody.velocity.y);
-        //This method of movement doesn't use Rigidbody2D or the physics system :(
-        //transform.Translate(0.1f * horizontalInput, 0 , 0);
-
-        Debug.Log("Horizontal Input: " + horizontalInput);
-
-        if (Input.GetButtonDown("Jump"))
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isOnGround)
         {
-            //Crappy jump logic 
-            transform.Translate(0, 5, 0);
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpStrength);
+            isOnGround = false;
+
         }
     }
+
+    private void Move()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        myRigidbody.velocity = new Vector2(horizontalInput * movementSpeed, myRigidbody.velocity.y);
+    }
 }
+
+//Don't use Input.Getkey, use GetButton and GetAxis.
+
+//if (Input.GetKey(KeyCode.D))
+//{
+//    transform.Translate(new Vector3(0.1f, 0, 0));
+//}
+
+//if (Input.GetKey(KeyCode.A))
+//{
+//    transform.Translate(new Vector3(-0.1f, 0, 0));
+//}
+
+//from Update(): transform.Translate(0.1f * horizontalInput, 0 , 0);
