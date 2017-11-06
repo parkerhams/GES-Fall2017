a3c3fd086ec3a332d946c43ecc9308755eccd9be
Parker Hamilton <PARKER.HAMILTON@iam.local>
2017-11-06 17:40:06 -0600

    Added Screens, Door Level Spawns, Fixing Wall Jump

---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour
+{
+    public void StartButtonClick()
+    {
+        SceneManager.LoadScene("Demo Scene");
+    }
+
+    public void CreditsButtonClicked()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    //Maybe attach that to an individual credits screen transition script...
+    public void TitleButtonClicked()
+    {
+        SceneManager.LoadScene("Title");
+    }
+}
