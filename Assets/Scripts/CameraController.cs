e3223f99ce98a79c6bbaa206c50fdaaad25dc6ee
Parker Hamilton <PARKER.HAMILTON@iam.local>
2017-11-06 18:26:21 -0600

    Camera Boundaries

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -15,11 +15,22 @@ public class CameraController : MonoBehaviour {
     [SerializeField]
     float yOffset;
 
+    [SerializeField]
+    float xBoundary;
+    [SerializeField]
+    float yBoundary;
+    [SerializeField]
+    Transform xBoundaryX;
+    [SerializeField]
+    Transform yBoundaryY;
+    //ALSO set upper and lower bounds
+
     float zOffset = -10;
 	// Use this for initialization
 	void Start ()
     {
-		
+        xBoundary = xBoundaryX.position.x;
+        yBoundary = yBoundaryY.position.y;
 	}
 	
 	// Update is called once per frame
@@ -28,6 +39,14 @@ public class CameraController : MonoBehaviour {
         Vector3 newPosition = new Vector3(objectToFollow.position.x + xOffset, objectToFollow.position.y + yOffset, zOffset);
         transform.position = Vector3.Lerp(transform.position, newPosition, cameraFollowSpeed*Time.deltaTime);
 
+        if (transform.position.x < xBoundary)
+        {
+            transform.position = new Vector3(xBoundary, transform.position.y, transform.position.z);
+        }
 
-	}
+        if (transform.position.y < yBoundary)
+        {
+            transform.position = new Vector3(transform.position.x, yBoundary, transform.position.z);
+        }
+    }
 }
